14a672971bfc8ac04b1b35bcdf6cc575e16ed8c5
Damian Tulacz <damian.tulacz@gmail.com>
2024-06-10 18:35:26 +0200

    updated main layout, added toggle button between list and map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MapComponent } from './map/map.component';
 import { ObjectsListComponent } from './objects-list/objects-list.component';
 import { FiltersComponent } from './filters/filters.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -15,9 +16,26 @@ import { FiltersComponent } from './filters/filters.component';
     FooterComponent,
     MapComponent,
     ObjectsListComponent,
-    FiltersComponent
+    FiltersComponent,
+    MatIconModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private renderer: Renderer2) {}
+
+  @ViewChild('objectsList') objectsList!: ElementRef<HTMLElement>;
+
+  displayList = true;
+
+  onToggle() {
+    if (this.displayList) {
+      this.displayList = false;
+      this.renderer.setStyle(this.objectsList.nativeElement, 'zIndex', '0');
+    } else {
+      this.displayList = true;
+      this.renderer.setStyle(this.objectsList.nativeElement, 'zIndex', '1');
+    }
+  }
+}
