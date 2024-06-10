9a358345e57ce4c78d5724ee1c6ce8a1075a9100
Damian Tulacz <damian.tulacz@gmail.com>
2024-06-10 18:04:56 +0200

    added main layout app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MapComponent } from './map/map.component';
+import { ObjectsListComponent } from './objects-list/objects-list.component';
+import { FiltersComponent } from './filters/filters.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    MapComponent,
+    ObjectsListComponent,
+    FiltersComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
