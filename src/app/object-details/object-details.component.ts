a856e18f4b20cacf6884870907e61bf3e7c27228
Damian Tulacz <damian.tulacz@gmail.com>
2024-06-10 16:57:39 +0200

    added object details component

---
+++ src/app/object-details/object-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-object-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './object-details.component.html',
+  styleUrl: './object-details.component.scss'
+})
+export class ObjectDetailsComponent {
+
+}
