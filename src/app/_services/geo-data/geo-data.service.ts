230828623cf42fcf27be23d43410adc69cfe87ce
Damian Tulacz <damian.tulacz@gmail.com>
2024-06-14 15:20:49 +0200

    updated object add component

--- src/app/_services/geo-data/geo-data.service.ts
+++ src/app/_services/geo-data/geo-data.service.ts
@@ -2,10 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Coordinate } from 'ol/coordinate';
-import { Observable, map } from 'rxjs';
+import { Observable, debounceTime, map } from 'rxjs';
 
 interface GeoData {
-  city: string;
+  place: string;
   countryName: string;
 }
 
@@ -15,20 +15,52 @@ interface GeoData {
 export class GeoDataService {
   constructor(private http: HttpClient) {}
 
-  private getUrl(coordinateLatLon: Coordinate) {
-    return `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coordinateLatLon[0]}&longitude=${coordinateLatLon[1]}&localityLanguage=en`;
+  // private getUrl(coordinateLatLon: Coordinate) {
+  //   return `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coordinateLatLon[0]}&longitude=${coordinateLatLon[1]}&localityLanguage=en`;
+  // }
+
+  private getReverseUrl(coordinateLatLon: Coordinate): string {
+    return `https://nominatim.openstreetmap.org/reverse?lat=${coordinateLatLon[0]}&lon=${coordinateLatLon[1]}&format=json`;
+  }
+
+  private getAddressesUrl(searchQuery: string): string {
+    // 'eiffel+tower'
+    return `https://nominatim.openstreetmap.org/search?q=${searchQuery}&format=json&accept-language=en`;
   }
 
   getGeoData(coordinateLatLon: Coordinate): Observable<GeoData> {
-    return this.http.get<any>(this.getUrl(coordinateLatLon)).pipe(
-      map(({ city, countryName, coordinate }) => {
+    return this.http.get<any>(this.getReverseUrl(coordinateLatLon)).pipe(
+      map((data) => {
+        if (data.error) return data.error;
         const newData = {
-          city,
-          countryName,
-          coordinate,
+          place:
+            data.address.city ||
+            data.address.town ||
+            data.address.municipality ||
+            data.address.village ||
+            data.address.county ||
+            data.address.state,
+          countryName: data.address.country,
         };
         return newData;
       })
     );
+  }
+
+  getAddresses(searchQuery: string): Observable<any> {
+    return this.http
+      .get<any>(this.getAddressesUrl(searchQuery))
+      .pipe(
+        map((addresses) => {
+          return addresses.map(({ display_name, lat, lon, place_id }: any) => {
+            return {
+              displayName: display_name,
+              lat,
+              lon,
+              placeId: place_id,
+            };
+          });
+        })
+      );
   }
 }
--- src/app/object-add/object-add.component.ts
+++ src/app/object-add/object-add.component.ts
@@ -3,12 +3,27 @@ import { BackdropComponent } from '../backdrop/backdrop.component';
 import {
   FormBuilder,
   FormGroup,
+  FormsModule,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+import { debounceTime } from 'rxjs';
+
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
 import { ObjectCategory, ObjectData } from '../_models/objectData';
 import { MapService } from '../_services/map/map.service';
+import { GeoDataService } from '../_services/geo-data/geo-data.service';
+import { ObjectsService } from '../_services/objects/objects.service';
 
 import Map from 'ol/Map';
 import { Coordinate } from 'ol/coordinate';
@@ -18,29 +33,40 @@ import VectorLayer from 'ol/layer/Vector';
 import { Geometry, Point } from 'ol/geom';
 import Feature from 'ol/Feature';
 import { Icon, Style } from 'ol/style';
-import { GeoDataService } from '../_services/geo-data/geo-data.service';
-import { ObjectsService } from '../_services/objects/objects.service';
-import { Router } from '@angular/router';
+import { easeOut } from 'ol/easing';
 
 @Component({
   selector: 'app-object-add',
   standalone: true,
-  imports: [BackdropComponent, ReactiveFormsModule, CommonModule],
+  imports: [
+    BackdropComponent,
+    ReactiveFormsModule,
+    CommonModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatIconModule,
+    FormsModule,
+    MatAutocompleteModule,
+  ],
   templateUrl: './object-add.component.html',
   styleUrl: './object-add.component.scss',
 })
 export class ObjectAddComponent {
   @ViewChild('mapContainer') mapContainer: ElementRef | undefined;
-  stage: 1 | 2 = 1;
+
   addObjectForm1stage!: FormGroup;
   addObjectForm2stage!: FormGroup;
+  searchForm!: FormGroup;
+
+  searchValuePlaces: any[] = [];
 
   country: string | undefined;
-  city: string | undefined;
-  coordinateLonLat: Coordinate | undefined;
-  private googleMapsUrl: string | undefined;
+  place: string | undefined;
+  coordinateLonLat!: Coordinate;
   private username: string | undefined;
-  private userId: string | undefined;
 
   private map: Map | undefined;
   private vectorSource: VectorSource | undefined;
@@ -56,15 +82,21 @@ export class ObjectAddComponent {
     private router: Router
   ) {}
 
-  ngAfterViewInit() {
+  ngOnInit() {
+    this.createSearchForm();
     this.createForm1stage();
+    this.createForm2stage();
+  }
+
+  ngAfterViewInit() {
+    this.initMap();
   }
 
   ngAfterViewChecked() {
     this.map?.setTarget(this.mapContainer?.nativeElement);
   }
 
-  createForm1stage(): void {
+  initMap(): void {
     this.map = this.mapService.getNewMap();
     this.map.setTarget(this.mapContainer?.nativeElement);
     this.map.getView().setMaxZoom(18);
@@ -76,17 +108,90 @@ export class ObjectAddComponent {
     this.map?.addLayer(this.vectorLayer);
 
     this.map.on('singleclick', (clickEvent) => {
-      this.coordinateLonLat = toLonLat(clickEvent.coordinate);
-
-      this.createMarker(clickEvent.coordinate);
-
-      this.geoDataService
-        .getGeoData([this.coordinateLonLat[1], this.coordinateLonLat[0]])
-        .subscribe(({ countryName, city }) => {
-          this.country = countryName;
-          this.city = city;
-        });
+      this.clearSearchForm();
+      this.setNewPoint(clickEvent.coordinate);
     });
+  }
+
+  createForm1stage(): void {
+    this.addObjectForm1stage = this.formBuilder.group({
+      coordinateLon: [{ value: '', disabled: true }, Validators.required],
+      coordinateLat: [{ value: '', disabled: true }, Validators.required],
+    });
+  }
+
+  createSearchForm(): void {
+    this.searchForm = this.formBuilder.group({
+      search: [''],
+    });
+
+    this.searchForm.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe(({ search }) => {
+        this.onSearch(search);
+      });
+  }
+
+  clearSearchForm(): void {
+    if (!this.searchForm.value && this.searchValuePlaces.length === 0) return;
+    this.searchForm.get('search')?.setValue('');
+    this.searchValuePlaces = [];
+  }
+
+  isClearBtnDisabled(): boolean {
+    if (this.searchValuePlaces.length !== 0 || this.searchForm.value !== '') {
+      return false;
+    }
+    return true;
+  }
+
+  onSearch(searchQuery: string): void {
+    if (!searchQuery) {
+      this.searchValuePlaces = [];
+      return;
+    }
+    this.geoDataService.getAddresses(searchQuery).subscribe((result: any[]) => {
+      this.searchValuePlaces = result;
+
+      if (result.length === 0) {
+        this.searchForm.get('search')?.setErrors({ noPlaceFound: true });
+        this.searchForm.get('search')?.markAsTouched();
+      }
+    });
+  }
+
+  getOptionText(option: any) {
+    return option.displayName;
+  }
+
+  onSearchPlaceSelect(event: any): void {
+    const lon = +event.option.value.lon;
+    const lat = +event.option.value.lat;
+    const coordinate = fromLonLat([lon, lat]);
+    const view = this.map?.getView();
+    view?.animate(
+      {
+        center: coordinate,
+        zoom: 14,
+        duration: 500,
+        easing: easeOut,
+      },
+      (isAnimationFinished: boolean) => {
+        if (isAnimationFinished) {
+          this.setNewPoint(coordinate);
+        }
+      }
+    );
+  }
+
+  setNewPoint(coordinate: Coordinate): void {
+    this.coordinateLonLat = toLonLat(coordinate);
+    this.addObjectForm1stage.setValue({
+      coordinateLon: this.coordinateLonLat[0].toString(),
+      coordinateLat: this.coordinateLonLat[1].toString(),
+    });
+    this.createMarker(coordinate);
+    this.getLocationData();
   }
 
   createMarker(coordinate: Coordinate) {
@@ -105,6 +210,26 @@ export class ObjectAddComponent {
 
     markerFeature.setStyle(markerStyle);
     this.vectorSource?.addFeature(markerFeature);
+  }
+
+  getLocationData(): void {
+    this.geoDataService
+      .getGeoData([this.coordinateLonLat[1], this.coordinateLonLat[0]])
+      .subscribe(({ countryName, place }) => {
+        this.country = countryName || 'unknown';
+        this.place = place || 'unknown';
+        this.addObjectForm2stage.get('location')?.setValue({
+          country: this.country,
+          place: this.place,
+        });
+      });
+  }
+
+  isValid1stage() {
+    return (
+      !!this.addObjectForm1stage.value.coordinateLon &&
+      !!this.addObjectForm1stage.value.coordinateLat
+    );
   }
 
   createForm2stage(): void {
@@ -134,9 +259,9 @@ export class ObjectAddComponent {
           },
           [Validators.required],
         ],
-        city: [
+        place: [
           {
-            value: this.city,
+            value: this.place,
             disabled: true,
           },
           [Validators.required],
@@ -146,32 +271,29 @@ export class ObjectAddComponent {
     });
   }
 
-  onNext(): void {
-    this.createForm2stage();
-    this.stage = 2;
-    this.googleMapsUrl = this.getGoogleMapsUrl(this.coordinateLonLat!);
-    console.log(this.googleMapsUrl);
-  }
-
-  onBack(): void {
-    this.stage = 1;
-  }
-
   onSubmit(): void {
-    const formRawValue = this.addObjectForm2stage.getRawValue();
+    const form1Value = this.addObjectForm1stage.value;
+    const coordinate = [
+      Number(form1Value.coordinateLon),
+      Number(form1Value.coordinateLat),
+    ];
+
+    const form2RawValue = this.addObjectForm2stage.getRawValue();
     const newObject: ObjectData = {
-      ...formRawValue,
+      ...form2RawValue,
       username: 'newuser',
       location: {
-        ...formRawValue.location,
-        coordinateLonLat: this.coordinateLonLat,
+        ...form2RawValue.location,
+        coordinateLonLat: coordinate,
       },
     };
+    if (!newObject.imageUrl) {
+      delete newObject.imageUrl;
+    }
     this.objectsService.addNewObject(newObject);
   }
 
-  getGoogleMapsUrl(coordinateLonLat: Coordinate): string {
-    // .../lat,lng
-    return `https://www.google.com/maps/place/${coordinateLonLat[1]},${coordinateLonLat[0]}`;
+  onClose(): void {
+    this.router.navigate(['/']);
   }
 }
