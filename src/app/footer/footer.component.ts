3df9fb5fa0f79d893a2cf18d32bdbb33b44db3d8
Damian Tulacz <damian.tulacz@gmail.com>
2024-06-14 22:40:08 +0200

    updated footer styling

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatTooltipModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  onGithubClick() {
+    window.open('https://github.com/Damiant94', '_blank');
+  }
 
+  onLinkedinClick() {
+    window.open('https://www.linkedin.com/in/damiantulacz/', '_blank');
+  }
 }
