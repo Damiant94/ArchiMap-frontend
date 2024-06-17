51af16a7e33fcc864d9c8b7183ff6da4e624e392
Damian Tulacz <damian.tulacz@gmail.com>
2024-06-17 21:59:29 +0200

    added lazy loading to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { ObjectDetailsComponent } from './object-details/object-details.component';
-import { ObjectAddComponent } from './object-add/object-add.component';
 
 export const routes: Routes = [
-    { path: 'details/:id', component: ObjectDetailsComponent },
-    { path: 'add', component: ObjectAddComponent },
+    { path: 'details/:id', loadComponent: () => import ('./object-details/object-details.component').then((m) => m.ObjectDetailsComponent) },
+    { path: 'add', loadComponent: () => import ('./object-add/object-add.component').then((m) => m.ObjectAddComponent) },
 ];
