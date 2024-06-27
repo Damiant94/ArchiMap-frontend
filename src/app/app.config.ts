aaae8e53d3878a54a9c7714a08c0ce2297b607c4
Damian Tulacz <damian.tulacz@gmail.com>
2024-06-27 23:08:41 +0200

    added service worker to cache endpoint for get object by id

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { feedInterceptor } from './_interceptors/feed/feed.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(
-      withInterceptors([feedInterceptor])
-    )
+    provideHttpClient(withInterceptors([feedInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
